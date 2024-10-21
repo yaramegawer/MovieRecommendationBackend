eeddcb659c672ed4581c597202b0a35652b7d5e9
Yara Sayed <yarasayed@gmail.com>
2024-10-22 01:51:05 +0300

    Initial commit of Movie Recommendation Backend

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const cors=require('cors');
 // Initialize app and load environment variables
 dotenv.config();
 const app = express();
-app.use(cors());
 
 // Connect to the database
 connectDB();
+app.use(cors());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
 
 // Middleware to parse JSON requests
 app.use(express.json());
